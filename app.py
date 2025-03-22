abfdab0c12a316b514750d78e5b0aae0e1e93cda
LidaSokha03 <sokha.pn@ucu.edu.ua>
2025-03-22 16:34:06 +0200

    for fun

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-pass
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'homepage'
+
+@app.route('/registration')
+def user_registration():
+    return 'registration'
+
+if __name__ == '__main__':
+    app.run()
